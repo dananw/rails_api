584d8bf48309cac0f7861df59610ce3d2aa0a099
Danan Wijaya <dananwijaya1996@gmail.com>
2020-09-16 22:27:08 +0700

    added login by facebook

---
+++ config/initializers/koala.rb
@@ -0,0 +1,6 @@
+Koala.configure do |config|
+  config.app_id = "365512871300543"
+  config.app_secret = "cead1f910059164b5a67149eb9e46b44"
+  # See Koala::Configuration for more options, including details on how to send requests through
+  # your own proxy servers.
+end
